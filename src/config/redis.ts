ae9f3a672df67b5a4b533af8cc6f329a0f6bdc4b
NannuriManoj <nannurimanoj26@gmail.com>
2026-04-09 12:11:13 +0530

    fix: set to graceful shutdown of redis

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -3,6 +3,10 @@ import { Redis } from "ioredis";
 export const redis = new Redis({
     host: process.env.REDIS_HOST || "localhost",
     port: Number(process.env.REDIS_PORT) || 6379,
+    password: process.env.REDIS_PASSWORD || undefined,
+    lazyConnect: true,
+    maxRetriesPerRequest: 1,
+    enableOfflineQueue: false,
 });
 
 redis.on("connect", () => {
