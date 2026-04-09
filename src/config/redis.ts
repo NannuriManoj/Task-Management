08757a1a2f09540889241bf9410180ef3ec62d15
NannuriManoj <nannurimanoj26@gmail.com>
2026-04-09 18:17:02 +0530

    fix: added the user for redis

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -3,6 +3,7 @@ import { Redis } from "ioredis";
 export const redis = new Redis({
     host: process.env.REDIS_HOST || "localhost",
     port: Number(process.env.REDIS_PORT) || 6379,
+    username: process.env.REDIS_USERNAME || undefined,
     password: process.env.REDIS_PASSWORD || undefined,
     lazyConnect: true,
     maxRetriesPerRequest: 1,
