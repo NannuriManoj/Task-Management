f87ac28471e4204330d06394b0ad2ea6fc46b18d
NannuriManoj <nannurimanoj26@gmail.com>
2026-04-17 15:43:16 +0530

    feat: added the instance of redis for BullMQ

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,5 +1,6 @@
 import { Redis } from "ioredis";
 import { env } from "./env.js"
+import { error } from "console";
 
 export const redis = new Redis({
     host: env.REDIS_HOST || "localhost",
@@ -11,6 +12,15 @@ export const redis = new Redis({
     enableOfflineQueue: false,
 });
 
+export const bullMQRedis = new Redis({
+    host: env.REDIS_HOST || "localhost",
+    port: Number(env.REDIS_PORT) || 6379,
+    username: env.REDIS_USERNAME || undefined,
+    password: env.REDIS_PASSWORD || undefined,
+    maxRetriesPerRequest: null,
+    retryStrategy: (times) => Math.min(times * 200, 10000),
+})
+
 export async function checkRedisConnection() {
   await redis.connect();
   await redis.ping();
@@ -18,11 +28,19 @@ export async function checkRedisConnection() {
 }
 
 redis.on("connect", () => {
-    console.log("Connected to Redis");
+    console.log("Connected to General use Redis...");
 });
 
 redis.on("error", (err) => {
-    console.error("Redis error:", err);
+    console.error("General use Redis error: ", err);
+});
+
+bullMQRedis.on("connect", ()=>{
+    console.log("Connected to BullMQ Redis...");
+});
+
+bullMQRedis.on("error", (err)=>{
+    console.error("BullMQ redis error: ", err);
 });
 
 export default redis;
